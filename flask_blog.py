c410668ffdb1b61f4311356de9254210149b3507
fulong <834424581@qq.com>
2017-05-19 18:45:22 +0800

    第二章

--- flask_blog.py
+++ flask_blog.py
@@ -1,15 +1,47 @@
 from flask import Flask,render_template
+from flask_script import Manager
+from flask_moment import Moment
+from flask_bootstrap import Bootstrap
+from datetime import datetime
+
+from flask_wtf import FlaskForm
+from wtforms import StringField,SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask (__name__)
+app.config['SECRET_KEY']='hard to guess string' # 可以用来存储框架，扩展，程序等的配置变量
 
 
-@app.route ('/<name>')
-def index(name):
-	return render_template('index.html',name=name)
+
+class NameForm(FlaskForm):
+	name = StringField('姓名',validators=[DataRequired()])
+	submit = SubmitField('提交')
+
+
+bootstrap = Bootstrap(app)
+moment = Moment(app)
+@app.route ('/',methods=['get','post'])
+def index1():
+	name=None
+	form=NameForm()
+	if form.validate_on_submit():
+		name=form.name.data
+		form.name.data=''
+	return render_template('index.html',name=name,form=form,current_time = datetime.utcnow())
+
 
 @app.route ('/user/<name>')
-def user(name):
-	return render_template('user.html',name=name)
+def index(name):
+	return render_template('user.html',name= name)
+
+@app.errorhandler(404)
+def page_not_find(e):
+	return render_template('404.html'),404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+	return render_template('500.html'),500
+
 
 if __name__ == '__main__':
 	app.run (debug=True)
