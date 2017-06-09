c325eeea70782beef319ef15b0e81d78fa5ec774
ErickLONG <834424581@qq.com>
2017-06-09 09:18:36 +0800

    第十五章 测试 获取代码覆盖报告

--- app/api_1_0/users.py
+++ app/api_1_0/users.py
@@ -1,5 +1,3 @@
-#！/usr/bin/env python
-# -*- coding:utf-8 -*-
 from flask import jsonify, request, current_app, url_for
 from . import api
 from ..models import User, Post
--- manage.py
+++ manage.py
@@ -6,6 +6,12 @@ from app.models import User,Role,Post,Follow,Permission,Comment
 from flask_script import Manager,Shell
 from flask_migrate import Migrate,MigrateCommand
 
+COV = None
+if os.environ.get('FLASK_COVERAGE'):
+	import coverage
+	COV = coverage.coverage(branch=True,include='app/*')
+	COV.start()
+
 app=create_app(os.getenv('FLASK_CONFIG')or 'default')
 manager = Manager(app)
 # 数据库迁移
@@ -16,14 +22,28 @@ def make_shell_context():
 	return dict(app=app, db=db, User=User, Role=Role,Post=Post,Follow=Follow,Permission=Permission,Comment=Comment)
 manager.add_command("shell", Shell(make_context=make_shell_context))
 
-
-
 @manager.command
-def test():
+def test(coverage=False):
 	'''启动单元测试'''
+	if coverage and not os.environ.get('FLASK_COVERAGE'):
+		import sys
+		os.environ['FLASK_COVERAGE']='1'
+		os.execvp(sys.executable,[sys.executable] + sys.argv)# 设定完环境变量重启脚本
+
 	import unittest
 	tests=unittest.TestLoader().discover('tests')
 	unittest.TextTestRunner(verbosity=2).run(tests)
+
+	if COV:
+		COV.stop()
+		COV.save()
+		print('覆盖总计：')
+		COV.report()
+		basedir = os.path.abspath(os.path.dirname(__file__))
+		covdir = os.path.join(basedir,'tmp/coverage')
+		COV.html_report(directory=covdir)
+		print('HTML 版本：file://%s/index.html'% covdir)
+		COV.erase()
 
 if __name__ == '__main__':
 	manager.run()
